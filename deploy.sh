6230b969591724f47361688fd101d7b0a40a43c2
Tom Herman <38570442+TomHerman11@users.noreply.github.com>
2022-12-16 19:43:48 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,16 @@
 # abort on errors
 set -e
 
-# build
 env PUBLIC_URL=https://tomherman11.github.io/us-states-quiz yarn build
 
-# navigate into the build output directory
 cd build
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy build output to github pages'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:TomHerman11/us-states-quiz.git master:gh-pages
-
-cd -
